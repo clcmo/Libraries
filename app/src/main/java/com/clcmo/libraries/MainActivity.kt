4009b780f1b1a3294c779bd973363af726a013b5
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2022-02-25 17:50:08 -0300

    Initial commit

---
+++ app/src/main/java/com/clcmo/libraries/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.clcmo.libraries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
